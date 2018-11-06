8655c475b8158bb64aa6f169af1da706a37693ba
lieselottezz <59070141@it.kmitl.ac.th>
2018-11-07 03:56:39 +0700

    Adding prototype of gridview image selector and in-app camera

--- Foodagram/app/src/main/java/com/example/foodagramapp/foodagram/MainActivity.java
+++ Foodagram/app/src/main/java/com/example/foodagramapp/foodagram/MainActivity.java
@@ -3,11 +3,19 @@ package com.example.foodagramapp.foodagram;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.foodagramapp.foodagram.Post.CameraFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.main_view, new CameraFragment())
+                    .commit();
+        }
     }
 }
---
+++ Foodagram/app/src/main/java/com/example/foodagramapp/foodagram/Post/PostActivity.java
@@ -0,0 +1,140 @@
+package com.example.foodagramapp.foodagram.Post;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.example.foodagramapp.foodagram.R;
+import com.example.foodagramapp.foodagram.Utils.Permissions;
+import com.example.foodagramapp.foodagram.Utils.SectionsPagerAdapter;
+
+//import com.example.foodagramapp.foodagram.Utils.Permissions;
+
+public class PostActivity extends AppCompatActivity {
+    private static final String TAG = "PostActivity";
+
+    //constants
+    private static final int ACTIVITY_NUM = 2;
+    private static final int VERIFY_PERMISSIONS_REQUEST = 1;
+
+    private ViewPager mViewPager;
+
+    private Context mContext = PostActivity.this;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+        Log.d(TAG, "onCreate: started.");
+        if(checkPermissionsArray(Permissions.PERMISSIONS)){
+            setupViewPager();
+        }else{
+            verifyPermissions(Permissions.PERMISSIONS);
+        }
+
+    }
+
+    /**
+     * return the current tab number
+     * 0 = GalleryFragment
+     * 1 = PhotoFragment
+     * @return
+     */
+    public int getCurrentTabNumber(){
+        return mViewPager.getCurrentItem();
+    }
+
+    /**
+     * setup viewpager for manager the tabs
+     */
+    private void setupViewPager(){
+        SectionsPagerAdapter adapter =  new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ImageSelectorFragment());
+        adapter.addFragment(new CameraFragment());
+
+        mViewPager = (ViewPager) findViewById(R.id.viewpager_container);
+        mViewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabsBottom);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        tabLayout.getTabAt(0).setText(getString(R.string.gallery));
+        tabLayout.getTabAt(1).setText(getString(R.string.photo));
+
+    }
+
+    public int getTask(){
+        Log.d(TAG, "getTask: TASK: " + getIntent().getFlags());
+        return getIntent().getFlags();
+    }
+
+    /**
+     * verifiy all the permissions passed to the array
+     * @param permissions
+     */
+    public void verifyPermissions(String[] permissions){
+        Log.d(TAG, "verifyPermissions: verifying permissions.");
+
+        ActivityCompat.requestPermissions(
+                PostActivity.this,
+                permissions,
+                VERIFY_PERMISSIONS_REQUEST
+        );
+    }
+
+    /**
+     * Check an array of permissions
+     * @param permissions
+     * @return
+     */
+    public boolean checkPermissionsArray(String[] permissions){
+        Log.d(TAG, "checkPermissionsArray: checking permissions array.");
+
+        for(int i = 0; i< permissions.length; i++){
+            String check = permissions[i];
+            if(!checkPermissions(check)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check a single permission is it has been verified
+     * @param permission
+     * @return
+     */
+    public boolean checkPermissions(String permission){
+        Log.d(TAG, "checkPermissions: checking permission: " + permission);
+
+        int permissionRequest = ActivityCompat.checkSelfPermission(PostActivity.this, permission);
+
+        if(permissionRequest != PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG, "checkPermissions: \n Permission was not granted for: " + permission);
+            return false;
+        }
+        else{
+            Log.d(TAG, "checkPermissions: \n Permission was granted for: " + permission);
+            return true;
+        }
+    }
+
+    /**
+     * BottomNavigationView setup
+     */
+//    private void setupBottomNavigationView(){
+//        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+//        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+//        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+//        BottomNavigationViewHelper.enableNavigation(mContext, this,bottomNavigationViewEx);
+//        Menu menu = bottomNavigationViewEx.getMenu();
+//        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+//        menuItem.setChecked(true);
+//    }
+}
