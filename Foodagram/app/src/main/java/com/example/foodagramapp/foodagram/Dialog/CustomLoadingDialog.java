a4405aa4bf1e6feb05a1f2954bc16dd79b06470a
Vasanchai <59070156@it.kmitl.ac.th>
2018-11-14 15:46:00 +0700

    [ADD] CustomLoadingDialog.class | Call in LoginFragment, RegisterFragment

---
+++ Foodagram/app/src/main/java/com/example/foodagramapp/foodagram/Dialog/CustomLoadingDialog.java
@@ -0,0 +1,23 @@
+package com.example.foodagramapp.foodagram.Dialog;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import com.example.foodagramapp.foodagram.R;
+import dmax.dialog.SpotsDialog;
+
+public class CustomLoadingDialog {
+
+    private AlertDialog loadingDialog;
+
+    public CustomLoadingDialog(Context context){
+        loadingDialog = new SpotsDialog.Builder().setContext(context).setTheme(R.style.CustomLoadingDialog).build();
+    }
+
+    public void showDialog(){
+        loadingDialog.show();
+    }
+
+    public void dismissDialog(){
+        loadingDialog.dismiss();
+    }
+}
--- Foodagram/app/src/main/java/com/example/foodagramapp/foodagram/MainActivity.java
+++ Foodagram/app/src/main/java/com/example/foodagramapp/foodagram/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.foodagramapp.foodagram;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
